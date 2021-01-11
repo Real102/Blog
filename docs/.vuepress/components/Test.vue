03add47eb85eeb0b156a88dff413decee3949952
WolfberryzZ <906368017@qq.com>
2021-01-11 10:41:11 +0800

    test：只是用来测试

---
+++ docs/.vuepress/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="test">
+		<img src="/img/avatar.png" alt="" />
+		<img src="../public/img/avatar.png" alt="" />
+		<img :src="$withBase('/img/avatar.png')" alt="" />
+		<div class="bgc bgc1"></div>
+		<div class="bgc bgc2"></div>
+	</div>
+</template>
+<script>
+export default {
+	name: "Test"
+}
+</script>
+<style lang="scss">
+.bgc {
+	width: 200px;
+	height: 200px;
+}
+.bgc1 {
+	background: url("/img/avatar.png") no-repeat;
+	background-size: 100% 100%;
+}
+.bgc2 {
+	background: url("../public/img/avatar.png") no-repeat;
+	background-size: 100% 100%;
+}
+</style>
